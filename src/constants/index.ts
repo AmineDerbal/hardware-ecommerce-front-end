d91d6c509df4b1ddd0b6c09944f87f5c47aebb5c
Amine derbal <aminedoran31@gmail.com>
2023-12-18 13:32:50 +0100

    Create constants and categories

---
+++ src/constants/index.ts
@@ -0,0 +1,33 @@
+const categories = [
+  {
+    name: 'PCs',
+    subCategories: ['Desktops', 'Laptops', 'Tablets', 'All-in-One'],
+  },
+  {
+    name: 'Components',
+    subCategories: [
+      'Power',
+      'CPUs',
+      'Mother Boards',
+      'GPUS',
+      'Storage',
+      'Memory',
+      'Casing',
+      'Cooling',
+    ],
+  },
+  {
+    name: 'Peripherals',
+    subCategories: [
+      'Monitors',
+      'Keyboards',
+      'Mouses',
+      'Headsets',
+      'Webcams',
+      'Cables',
+      'Accessories',
+    ],
+  },
+];
+
+export { categories };
