80d77abbd92b71c439f34a11ab780d2b11a2be11
Amine derbal <aminedoran31@gmail.com>
2023-12-19 14:18:47 +0100

    Add Color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
+
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      gray: colors.gray,
+    },
     extend: {},
   },
   plugins: [],
