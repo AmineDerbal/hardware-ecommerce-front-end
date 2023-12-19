6a284e0e789a8ba0e791e395454ea577181d4641
Amine derbal <aminedoran31@gmail.com>
2023-12-19 14:57:54 +0100

    Update App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, Login, Register } from './pages';
+import { Header } from './components';
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
