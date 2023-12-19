2261d1286e1c65ad07b189495758f21afb964cf6
Amine derbal <aminedoran31@gmail.com>
2023-12-19 14:57:13 +0100

    Create Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,42 @@
+import { Badge } from '@material-ui/core';
+import { ShoppingCart, Search } from '@material-ui/icons';
+import { Person } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
+import { logo } from '../assets';
+
+const Header = () => {
+  return (
+    <div className="w-full ">
+      <div className="flex items-center justify-around px-6 py-2">
+        <a
+          href="/"
+          className="flex items-center"
+        >
+          <img
+            src={logo}
+            alt="logo"
+            className="w-32 h-32 object-contain"
+          />
+        </a>
+        <div className="flex items-center ml-[25px] p-[5px] border-solid border-[1px] border-gray-200">
+          <input placeholder="Search" />
+          <Search style={{ color: 'gray', fontSize: 16 }} />
+        </div>
+        <div className="flex items-center gap-6">
+          <Link to="/login">
+            {' '}
+            <Person />{' '}
+          </Link>
+          <Badge
+            badgeContent={4}
+            color="primary"
+          >
+            <ShoppingCart />
+          </Badge>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
