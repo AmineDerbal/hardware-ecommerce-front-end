61b5fe8ea381212d381b3c900081cdea101457ee
Amine derbal <aminedoran31@gmail.com>
2023-12-21 15:21:23 +0100

    Add connexion panel

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,16 @@
 import { Badge } from '@material-ui/core';
-import { ShoppingCart, Search } from '@material-ui/icons';
-import { Person } from '@material-ui/icons';
-import { Link } from 'react-router-dom';
+import {
+  ShoppingCartOutlined,
+  Search,
+  PersonOutline,
+} from '@material-ui/icons';
+import { useNavigate } from 'react-router-dom';
+import Navbar from './Navbar';
 import { logo } from '../assets';
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="w-full ">
       <div className="flex items-center justify-around px-6 py-2">
@@ -22,19 +28,39 @@ const Header = () => {
           <input placeholder="Search" />
           <Search style={{ color: 'gray', fontSize: 16 }} />
         </div>
-        <div className="flex items-center gap-6">
-          <Link to="/login">
-            {' '}
-            <Person />{' '}
-          </Link>
+
+        <div className="flex items-center justify-end gap-12">
+          <div className="relative">
+            <PersonOutline className="cursor-pointer" />
+            <div className="z-10 min-w-[200px] border-solid border-[1px] border-black absolute bg-white right-[-50px] shadow-xl border-0 border-t-2 border-red ">
+              <div className="flex flex-col p-4 gap-2 justify-center items-center">
+                <p>A returning customer?</p>
+                <button
+                  className="text-white p-1 bg-red w-fit"
+                  onClick={() => navigate('/Login')}
+                >
+                  Sign in
+                </button>
+                <p>Don't have an account?</p>
+                <a
+                  href="/Register"
+                  className="text-blue"
+                >
+                  Register
+                </a>
+              </div>
+            </div>
+          </div>
           <Badge
-            badgeContent={4}
-            color="primary"
+            badgeContent={0}
+            color="secondary"
+            showZero
           >
-            <ShoppingCart />
+            <ShoppingCartOutlined />
           </Badge>
         </div>
       </div>
+      <Navbar />
     </div>
   );
 };
