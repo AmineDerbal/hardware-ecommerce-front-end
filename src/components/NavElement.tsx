203149717486a1b4d941b54de634d7a31c1fc2d8
Amine derbal <aminedoran31@gmail.com>
2023-12-22 14:54:43 +0100

    Add navigation to category

--- src/components/NavElement.tsx
+++ src/components/NavElement.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ArrowDropDown } from '@material-ui/icons';
 import NavSubElement from './NavSubElement';
 type NavElementProps = {
@@ -7,12 +8,14 @@ type NavElementProps = {
 };
 
 const NavElement = ({ name, subCategories }: NavElementProps) => {
+  const navigate = useNavigate();
   const [showSubCategories, setShowSubCategories] = useState(false);
   return (
     <div
       className="relative py-4 cursor-pointer"
       onMouseEnter={() => setShowSubCategories(true)}
       onMouseLeave={() => setShowSubCategories(false)}
+      onClick={() => navigate(`/category/${name}`)}
     >
       <div className="flex gap-1 ">
         <p>{name}</p>
@@ -20,7 +23,7 @@ const NavElement = ({ name, subCategories }: NavElementProps) => {
       </div>
 
       {showSubCategories && (
-        <div className="absolute mt-4 min-w-[200px] border-solid border-[1px] border-gray-200 bg-white shadow-xl text-black z-10">
+        <div className="absolute mt-4 min-w-[150px] border-solid border-[1px] border-gray-200 bg-white shadow-xl text-black z-10">
           {subCategories.map((subCategory) => (
             <NavSubElement
               key={subCategory}
