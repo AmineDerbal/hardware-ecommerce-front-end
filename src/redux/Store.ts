60562f7d939a4b9ed0dfe1f7e4f80c65bd35e3ea
Amine derbal <aminedoran31@gmail.com>
2023-12-27 17:09:48 +0100

    Add user reducer to the store

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import toggleReducer from './toggle/toggleSlice';
+import userReducer from './user/userSlice';
 
 const store = configureStore({
   reducer: {
     toggle: toggleReducer,
+    user: userReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>; // Define and export RootState type
+export type AppDispatch = typeof store.dispatch;
 export default store;
