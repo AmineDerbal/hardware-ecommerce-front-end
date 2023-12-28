7c621e54b748223a5673e2f5047dea52d4b8313e
Amine derbal <aminedoran31@gmail.com>
2023-12-28 15:44:12 +0100

    Update user states when logging

--- src/redux/user/userSlice.ts
+++ src/redux/user/userSlice.ts
@@ -5,8 +5,6 @@ export const fetchUser = createAsyncThunk(
   'user/fetchUser',
   async ({ username, password }: { username: string; password: string }) => {
     try {
-      console.log('fetchUser');
-      console.log(`${process.env.REACT_APP_API_URL}auth/login`);
       const response = await axios.post(
         `${process.env.REACT_APP_API_URL}auth/login`,
         {
@@ -14,7 +12,6 @@ export const fetchUser = createAsyncThunk(
           password,
         },
       );
-      console.log(response.data);
       return response.data;
     } catch (error) {
       return error;
@@ -26,12 +23,44 @@ const initialState = {
   username: null as string | null,
   email: null as string | null,
   accessToken: null as string | null,
+  isUserLoggedIn: false,
+  isLodaing: false,
+  isError: false,
 };
 
 const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUser.pending, (state) => {
+      const isLodaing = true;
+      const isError = false;
+      return { ...state, isLodaing, isError };
+    });
+    builder.addCase(fetchUser.fulfilled, (state, action) => {
+      const isLodaing = false;
+      const isError = false;
+      const isUserLoggedIn = true;
+      const { username, email, accessToken } = action.payload;
+      const currentUser = username;
+      return {
+        ...state,
+        isLodaing,
+        isError,
+        isUserLoggedIn,
+        username,
+        currentUser,
+        email,
+        accessToken,
+      };
+    });
+    builder.addCase(fetchUser.rejected, (state) => {
+      const isLodaing = false;
+      const isError = true;
+      return { ...state, isLodaing, isError };
+    });
+  },
 });
 
 export default userSlice.reducer;
