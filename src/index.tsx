e52c83758da6ea1054714cf64419ee594abdd6c9
Amine derbal <aminedoran31@gmail.com>
2023-12-28 15:43:33 +0100

    Warp comopnent with PersistGate

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { CssBaseline } from '@material-ui/core';
+import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import store from './redux/Store';
+import { persistor } from './redux/Store';
 import './index.css';
 
 const root = ReactDOM.createRoot(
@@ -12,8 +14,13 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <CssBaseline />
-      <App />
+      <PersistGate
+        loading={null}
+        persistor={persistor}
+      >
+        <CssBaseline />
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 );
