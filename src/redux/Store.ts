7f9e7dba3e2908674e15c4df1b3e2ed4b649cb7e
Amine derbal <aminedoran31@gmail.com>
2023-12-28 15:43:08 +0100

    configure redux persist

--- src/redux/Store.ts
+++ src/redux/Store.ts
@@ -1,14 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import toggleReducer from './toggle/toggleSlice';
 import userReducer from './user/userSlice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+};
+
+const rootReducer = combineReducers({
+  toggle: toggleReducer,
+  user: userReducer,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: {
-    toggle: toggleReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   }),
 });
 
+export const persistor = persistStore(store);
 export type RootState = ReturnType<typeof store.getState>; // Define and export RootState type
 export type AppDispatch = typeof store.dispatch;
 export default store;
